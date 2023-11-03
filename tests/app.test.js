a4b3745181f34b70aa4413870a1cf57057a5d182
Dridr1 <a.driano@outlook.com>
2023-11-03 01:00:43 -0300

    chore: resolvidos erros nos testes e repositories. Adicionado script de testes e dev com nodemon

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,68 +4,75 @@ import db from "../src/db.js";
 
 const agent = supertest(app);
 
-beforeAll(async () => {
-    await db.connect();
-    await db.query(`INSERT INTO task VALUES ("levar minha vó no jiujitsu")`);
-    await db.query(`INSERT INTO task VALUES ("dar banho no peixe")`);
-    await db.query(`INSERT INTO task VALUES ("levar o cachorro pra prova")`);
-    await db.query(`INSERT INTO task VALUES ("regar o vaso")`);
-    await db.query(`INSERT INTO task VALUES ("não tankar e ir de base")`);
-    await db.query(`INSERT INTO task VALUES ("wardar a tribush do bot")`);
+
+afterAll(() => {
+    db.query(`TRUNCATE TABLE tasks RESTART IDENTITY`);
 });
 
 describe("todo list testes de integração", () => {
     describe("POST /add-task", () => {
         it("deve retornar CREATED", async () => {
             const task = { task: "dar banho no gato" };
-            let response;
-            await agent.post("/add-task").send(task).then(ans => response = ans.status);
-            expect(response).toEqual(201);
+            try {
+                const res = await agent.post("/add-task").send(task);
+                expect(res.statusCode).toEqual(201);
+            } catch (error) {
+                console.log(error);
+            }
         });
     });
 
     describe("GET /tasks", () => {
         it("deve retornar a lista de atividades", async () => {
-            let tasks;
-            await agent.get("/tasks").then(ans => tasks = ans.data);
-            expect(tasks.length > 0).toEqual(true);
+            try {
+                await agent.get("/tasks").expect(
+                    ans => {
+                        if (!(ans.body[0].task.includes("dar banho no gato"))) throw new Error("Atividades não retornadas");
+                    }
+                );
+                return;
+            } catch (error) {
+                throw new Error(error);
+            }
         });
     });
 
     describe("GET /task-by-id", () => {
         it("deve retornar uma atividade específica", async () => {
-            let task;
-            await agent.get("/task-by-id?id=1").then(ans => task = ans.data[0]);
-            
-            expect(task === "levar minha vó no jiujitsu").toEqual(true);
+            await agent.get("/task-by-id/1").expect(
+                ans => {
+                    if (!(ans.body[0].task.includes("dar banho no gato"))) throw new Error("Atividade não foi retornada");
+                }
+            );
         });
     });
 
-    describe("PUT /update-tast", () => {
+    describe("PUT /update-task", () => {
         it("deve alterar uma atividade", async () => {
-            let taskBefore;
-            await agent.get("/task-by-id?id=1").then(ans => taskBefore = ans.data[0]);
-            let taskAfter = "salvar a matéria";
-            await agent.put("/change-task?id=1").send({taskAfter});
-            
-            expect(taskBefore  !== taskAfter).toEqual(true);
+            try {
+                const taskBefore = (await agent.get("/task-by-id/1")).body[0].task;
+                const taskAfter = "Levar a vó no jiujitsu";
+                await agent.put("/update-task?id=1").send({ task: taskAfter });
+                expect((await agent.get("/task-by-id/1")).body[0].task === taskBefore).toEqual(false)
+            } catch (error) {
+                throw new Error(error);
+            }
         });
     });
 
     describe("DELETE /delete-task", () => {
         it("deve apagar uma atividade", async () => {
-            let tasksSize;
-            await agent.get("/tasks").then(ans => tasksSize = ans.data.length);
-            let afterSize;
-            await agent.delete("/delete-task?id=1").then(ans => afterSize = ans.data.length);
-
-            expect(afterSize < tasksSize).toEqual(true);
+            try {
+                const tasksSize = (await agent.get("/tasks")).body.length;
+                await agent.delete("/delete-task?id=1");
+                const afterSize = (await agent.get("/tasks")).body.length;
+                console.log({ tasksSize, afterSize });
+                expect(afterSize < tasksSize).toBeTruthy();
+            } catch (error) {
+                throw new Error(error);
+            }
         });
     });
 
-});
 
-afterAll(() => {
-    db.query(`DELETE FROM tasks WHERE 1=1`);
-    db.end();
 });
